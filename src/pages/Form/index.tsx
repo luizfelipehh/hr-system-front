fde55a6191c2ac0dcfa10b84de5227c39d9b703e
Luiz <lfheemann@hotmail.com>
2021-03-18 11:57:12 +1000

    Created front end project

---
+++ src/pages/Form/index.tsx
@@ -0,0 +1,95 @@
+import React, { useCallback, useEffect, useRef } from 'react'
+import { PageHeader, Button, Form, Input } from 'antd'
+import { useHistory } from 'react-router-dom'
+
+import VMasker from 'vanilla-masker'
+
+import axios from 'axios'
+
+const layout = {
+  labelCol: { span: 0 },
+  wrapperCol: { span: 4 },
+};
+
+const PageForm: React.FC = () => {
+  const [form] = Form.useForm();
+  const history = useHistory();
+  const onChangeSalary = useCallback((e: any) => {
+    const value = e.target.value;
+    let valueMask = VMasker.toMoney(value, {
+    // Decimal precision -> "90"
+    precision: 2,
+
+    // Decimal separator -> ",90"
+    separator: '.',
+
+    // Number delimiter -> "12.345.678"
+    delimiter: ',',
+
+    // Money unit -> "$ 12.345.678,90"
+    unit: '$',
+
+    });
+        
+    form.setFieldsValue(
+      { salary: valueMask }
+    );
+
+  }, []);
+
+  const onFinish = useCallback((values: any) => {
+    values.salary = VMasker.toNumber(values.salary);
+
+  const request = async () => {
+    try {
+      await axios.post("http://localhost:3333/employees", values);
+      history.push("/");
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  request();
+  }, []);
+
+  const onReset = useCallback(() => {
+    form.resetFields();
+    history.push("/");
+  }, [history]);
+
+  return (
+    <>
+      <PageHeader
+      className="site-page-header"
+      title="Bank Employee Onboarding Form"
+      />
+    
+    <div style={{ padding: 24 }}>
+      <Form {...layout} form={form} name="control-hooks" onFinish={onFinish}>
+        <Form.Item name="firstName" label="First Name" rules={[{ required: false }]}>
+          <Input />
+        </Form.Item>
+
+        <Form.Item name="lastName" label="Last Name" rules={[{ required: false }]}>
+          <Input />
+        </Form.Item>
+
+        <Form.Item name="salary" label="Salary" rules={[{ required: false }]}>
+          <Input onChange={onChangeSalary} />
+        </Form.Item>
+
+        <Form.Item>
+          <Button type="primary" htmlType="submit">
+            SUBMIT
+          </Button>
+          <Button style={{ marginLeft: 10 }} htmlType="button" onClick={onReset}>
+            CANCEL
+          </Button>
+
+        </Form.Item>
+      </Form>
+    </div>
+  </>
+);
+}
+
+export default PageForm;
---
+++ src/pages/Listing/index.tsx
@@ -0,0 +1,103 @@
+import React, { useCallback, useEffect, useState } from 'react'
+import { PageHeader, Button, Table, Space, Typography } from 'antd'
+import { useHistory } from 'react-router-dom'
+
+import axios from 'axios'
+
+import './style.css';
+
+const { Title } = Typography;
+
+const Listing: React.FC = () => {
+const history = useHistory();
+const [data, setData] = useState<any>([]);
+
+  useEffect((): void => {
+    const request = async () => {
+      try {
+        let result = await axios.get("http://localhost:3333/employees");
+        setData(result.data)
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    request();
+  }, []);
+
+  const onClick = useCallback((): void => {
+    history.push("/form");
+  }, [history]);
+
+  const onClickDetails = useCallback((record: any): void => {
+    history.push("/details", record);
+  }, [history]);
+
+  const onClickDelete = useCallback((record: any): void => {
+  const request = async () => {
+    try {
+      await axios.delete("http://localhost:3333/employees/"+record.id);
+
+      let result = await axios.get("http://localhost:3333/employees");
+
+      setData(result.data)
+
+    }catch (error) {
+      console.log(error);
+    }
+  }
+
+  request();
+  }, [history]);
+
+  const columns = [
+    {
+      title: 'Employee ID',
+      dataIndex: 'id',
+      key: 'id',
+    },
+    {
+      title: 'First Name',
+      dataIndex: 'firstName',
+      key: 'firstName',
+    },
+    {
+      title: 'Last Name',
+      dataIndex: 'lastName',
+      key: 'lastName',
+    },
+    {
+      title: 'View',
+      key: 'view',
+      render: (text: any, record: any) => (
+        <Space size="middle">
+          <Button onClick={() => onClickDetails(record)} type="link">View Details</Button>
+        </Space>
+      ),
+    },
+    {
+      title: 'Delete',
+      key: 'delete',
+      render: (text: any, record: any) => (
+        <Space style={{marginRight: 0}}>
+          <Button onClick={() => onClickDelete(record)} type="link">Delete and</Button>
+          <Button style={{marginLeft: -34}} onClick={() => onClick()} type="link">Create</Button>
+        </Space>
+      ),
+    },
+  ];
+
+  return (
+    <>
+      <PageHeader
+        className="site-page-header"
+        title="Employee Onboarding"
+      />
+      <div style={{ padding: 24 }}>
+        <Button style={{ marginBottom: 20 }} onClick={onClick} type="primary">PLEASE CREATE YOUR PROFILE</Button>
+        <Title level={3}>Your Details</Title>
+        <Table  columns={columns} rowKey={(record) => record.id} dataSource={data} />
+      </div>
+    </>
+  );
+}
+export default Listing;
